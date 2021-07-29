d1869b0fa709cad39a8295c2a5d1e5575aa6b42b
Felipe <felipebdruzian@gmail.com>
2021-07-29 19:36:00 -0300

    atividade complementar

---
+++ AlgeProg/Desafio2AtvAssncr/1/Atv1.c
@@ -0,0 +1,11 @@
+/*
+1. Construa um programa que receba os dados de compras de um individuo. O sistema deve
+receber Código da compra, descrição produto, quantidade e valor unitário (calcular o valor total).
+Regras do sistema:
+a. O programa deve solicitar para o usuário a informação do valor de limite das compras;
+b. Só poderão ser lançadas compras dentro do limite informado;
+c. O sistema deverá, ao final, apresentar os dados da compra de menor valor;
+d. O sistema deverá apresentar a descrição e valor unitário do produto de maior valor(considerar valor unitário);
+e. O sistema deverá apresentar o valor médio por compra;
+f. Caso o usuário ultrapasse o limite de compras, o sistema deverá apresentar o valor negativo deste cliente.
+*/
---
+++ AlgeProg/Desafio2AtvAssncr/2/Atv2.c
@@ -0,0 +1,6 @@
+/*
+2. Implemente um sistema que receba e armazene uma lista de 10 nomes completos, e:
+a. Apresente a lista de nomes na tela;
+b. Mostre a média de caracteres por nomes;
+c. Concatene, em uma variável distinta, o primeiro e ultimo nomes informados;
+*/
