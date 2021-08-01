74dc5b42fdc3dee424a91f3f189c1e5503e95d7b
Felipe <felipebdruzian@gmail.com>
2021-08-01 01:37:47 -0300

    Proj. Intr. 1

---
+++ IEC/ProjetoIntegrador1/pji1.c
@@ -0,0 +1,16 @@
+/*
+• Construção de um algoritmo e um software que calcule o troco
+mínimo:
+• Usuário deve fornecer valor do produto
+• Usuário deve fornecer o valor pago
+• Considerar moedas:
+0,01 e 0,05
+0,10 e 0,25
+0,50 e 1,00
+• Considerar notas:
+2 e 5
+10 e 20
+50, 100 e 200 R$
+• O programa deverá realizar o cálculo do troco do comprador, visando
+minimizar o total de notas e moedas fornecidas.
+*/
