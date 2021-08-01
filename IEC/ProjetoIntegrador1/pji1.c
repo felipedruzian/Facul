737119e73184931777bb40aa2808a42908a40875
Felipe <felipebdruzian@gmail.com>
2021-08-01 16:29:21 -0300

    pj1 v0.1

--- IEC/ProjetoIntegrador1/pji1.c
+++ IEC/ProjetoIntegrador1/pji1.c
@@ -14,3 +14,40 @@ mínimo:
 • O programa deverá realizar o cálculo do troco do comprador, visando
 minimizar o total de notas e moedas fornecidas.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    //variaveis
+    int n2, n5, n10, n20, n50, n100, n200;
+    float m01, m05, m10, m25, m50, m1;
+    float valorproduto, valorpago;
+    
+    //inicio
+    printf("Insira o valor do produto que deseja: ");
+    scanf("%f", &valorproduto);
+    printf("Insira o valor que deseja pagar: ");
+    scanf("%f", &valorpago);
+
+    //casos sem troco
+    if (valorpago == valorproduto) {
+        printf("Voce pagou o valor exato do produto, portanto nao tem troco.\n"); 
+    }
+    if (valorpago < valorproduto){
+        printf("O valor de pagamento e menor que o valor do produto!\n");
+        printf("Por favor, digite um novo valor: ");
+        scanf("%f", &valorpago);
+    }
+
+    //troco
+    
+
+    
+    
+    printf("\nFim do programa! \n");
+    system("pause");
+    return 0;
+}
