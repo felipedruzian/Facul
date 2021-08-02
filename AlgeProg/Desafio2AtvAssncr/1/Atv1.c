367f16262a857515d9b3f0bb03a0fd12500ea7c5
Felipe <felipebdruzian@gmail.com>
2021-08-01 22:22:11 -0300

    Atv1 v0.8

--- AlgeProg/Desafio2AtvAssncr/1/Atv1.c
+++ AlgeProg/Desafio2AtvAssncr/1/Atv1.c
@@ -11,17 +11,17 @@ f. Caso o usuário ultrapasse o limite de compras, o sistema deverá apresentar 
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 
 int main()
 {
     // variaveis
     float ValorLimiteCompras;
-    int x = 1; //quantidade de produtos
-    int y = 0; //numero do produto
+    int x = 32;  //quantidade de produtos
+    int y = 0;  //"numero" do produto
     int QuantidadeDoProduto[x];
-    float ValorDoProduto[x];
-    char codigo[x][16], descricao[x][32];
+    float ValorDoProduto[x], SomaValores, MediaCompras;
+    char codigo[x][16], descricao[x][32]; 
     
     // inicialização
     printf("----------- // Programa de Compras \\ ----------\n");
@@ -36,16 +36,17 @@ int main()
         printf("Valor Disponivel: %.2f \n", ValorLimiteCompras);
         printf("Digite o nome ou descricao do produto: ");
         scanf("%s", &descricao[y]);
-        //printf("Insira o codigo do produto: ");
-        //scanf("%s", &codigo[y]);
+        printf("Insira o codigo do produto: ");
+        scanf("%s", &codigo[y]);
         printf("Digite a quantidade do produto: ");
         scanf("%i", &QuantidadeDoProduto[y]);
         printf("Insira o valor do produto: ");
         scanf("%f", &ValorDoProduto[y]);
         printf("\n");
 
+        SomaValores += ValorDoProduto[y];
         ValorLimiteCompras = ValorLimiteCompras - QuantidadeDoProduto[y] * ValorDoProduto[y];
-        x++;
+        if(y > 30){x++;} //fingi q da certo, so loco
         y++;
     }
     
@@ -53,10 +54,26 @@ int main()
     if (ValorLimiteCompras <= 0) 
     {
         //printf("%i , %i \n", x, y);
-        printf("%.2f \n", ValorLimiteCompras);
+
+        //valor que ficou devendo
+        if (ValorLimiteCompras < 0) {
+            printf("Valor que voce ficou devendo: %.2f \n", ValorLimiteCompras);
+        }
+        /*
         for (int z = 0; z < y; z++){
             printf("%.2f \n", ValorDoProduto[z]);
-        }
+        } */
+
+        //media
+        MediaCompras = SomaValores/(y);
+        printf("Valor medio por compra: %.2f\n", MediaCompras);
+
+        //maior produto
+        
+
+        //menor produto
+
+
     }
     
 
