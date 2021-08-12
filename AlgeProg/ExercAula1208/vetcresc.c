74aad7efcf48720583ecbe1f46519c2d32b483ae
Felipe <felipebdruzian@gmail.com>
2021-08-12 18:04:54 -0300

    vetcresc v0.1

---
+++ AlgeProg/ExercAula1208/vetcresc.c
@@ -0,0 +1,19 @@
+/*
+2. Crie um programa que popule um vetor de inteiros de 10 posições.
+Ordene este vetor em ordem crescente e apresente o resultado na tela.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    
+    /* code */
+    
+
+    printf("Fim do programa!\n");
+    system("pause");
+    return 0;
+}
