294379c1201407e58c45276ae83be1bc7d399623
Felipe <felipebdruzian@gmail.com>
2021-08-12 17:29:29 -0300

    BinToDec v1.0

---
+++ AlgeProg/ExercAula1208/bintodec.c
@@ -0,0 +1,45 @@
+/*
+1. Implemente um programa que receba um valor binário de 8bits (vetor)
+e realize a conversão deste para decimal
+– apresente o resultado na tela.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    int bin[8], dec[8], num=0;
+
+    //entrada
+    printf("Digite um numero em binario dde 8bits: \n");
+    for (int i = 7; i > -1; i--) {
+        scanf("%i", &bin[i]);
+    }
+    
+    //processamento
+    for (int i = 7; i > -1; i--) {
+        dec[i] = (bin[i]*(pow(2,i)));
+    }
+    for (int i = 0; i < 8; i++) {
+        num += dec[i];
+    }
+    
+    
+    
+    //saida
+    printf("\nNumero Binario Digitado: ");
+    for (int i = 7; i > -1; i--) {
+        printf(" %i ", bin[i]);
+    }
+    
+    printf("\nNumero em Decimal:  ");
+    printf("%i\n", num);
+    
+
+
+    printf("\nFim do programa!\n");
+    system("pause");
+    return 0;
+}
